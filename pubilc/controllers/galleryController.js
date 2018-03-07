0f2dcbdee0e1db1ccff75faeb7fadf2623bb42cb
Karen Zaunscherb <kzaunsch@asu.edu>
2018-03-07 18:14:18 +1100

    Initial Upload

---
+++ pubilc/controllers/galleryController.js
@@ -0,0 +1,15 @@
+var galleryCtrl = angular.module('galleryCtrl', []);
+galleryCtrl.controller('galleryController', function($scope, $http){
+    $scope.planets = {};
+    console.log($scope.planets);
+    //Retrieve all the planets to show the gallery
+    $http.get('/planet')
+        .success(function(data){
+            console.log(JSON.stringify(data));
+            $scope.planets = data;
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+});
